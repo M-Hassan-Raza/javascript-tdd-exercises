c7476daf663583bb4ee75f30b5d5625f58bb8d87
hassan <hassan@nu.edu.pk>
2022-08-06 14:07:23 +0500

    completed all exercises

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,38 @@
-const caesar = function() {
+const caesar = function(sentence, shift) {
+    let cypher = '';
+    shift = shift % 27;
+    for(let i = 0; i < sentence.length; i++)
+    {
+        if(sentence[i] == ',' || sentence[i] == '.' || sentence[i] == '!' || sentence[i] == ' ')
+        {
+            cypher += sentence[i];
+        }
+        else{
+            let currentAscii = sentence.charCodeAt(i);
+            let newAscii = currentAscii + shift;
 
+            if(currentAscii >= 65 && currentAscii <= 90 && newAscii > 90)
+            {
+                newAscii = newAscii - 26;
+            }
+            else if(currentAscii >= 65 && currentAscii <= 90 && newAscii < 65)
+            {
+                newAscii = newAscii + 26;
+            }
+            else if(currentAscii >= 97 && currentAscii <= 122 && newAscii > 122)
+            {
+                newAscii = newAscii - 26;
+            }
+            else if(currentAscii >= 97 && currentAscii <= 122 && newAscii < 97)
+            {
+                newAscii = newAscii + 26;
+            }
+
+            cypher += String.fromCharCode(newAscii);
+        }
+    }
+
+    return cypher;
 };
 
 // Do not edit below this line
